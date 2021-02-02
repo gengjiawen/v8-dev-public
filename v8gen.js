4aa2ba9dbc1dcb4758c0201654049400ca804450
Jiawen Geng <technicalcute@gmail.com>
2021-02-02 06:17:58 -0800

    add helper script

---
+++ v8gen.js
@@ -0,0 +1,32 @@
+const { execSync } = require("child_process")
+
+const generateCmd = (cpuArch) => {
+  return `
+gn gen --export-compile-commands out/${cpuArch}.release \
+--args='is_component_build = false
+is_debug = false
+target_os = "linux"
+target_cpu = "${cpuArch}"
+v8_target_cpu = "${cpuArch}"
+v8_use_external_startup_data = false
+use_goma = false
+goma_dir = "None"
+v8_enable_backtrace = true
+v8_enable_disassembler = true
+v8_enable_object_print = true
+v8_enable_verify_heap = true'
+`
+}
+
+const execCmd = cmd => {
+  console.log(cmd)
+  try {
+    const out = execSync(cmd)
+    console.log(out.toString())
+  } catch (error) {
+    console.error(error.stderr.toString())
+  }
+}
+
+execCmd(generateCmd("arm64"))
+execCmd(generateCmd("x64"))
